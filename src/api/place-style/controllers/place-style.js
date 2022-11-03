85b13a4b2837c5ea3f681c96f94e942dea60cf75
nmammadov <n.mammadov@zirinc.com>
2022-11-03 12:18:28 +0400

    add translate

--- src/api/place-style/controllers/place-style.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  place-style controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::place-style.place-style');
--- src/api/place-style/services/place-style.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * place-style service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::place-style.place-style');
